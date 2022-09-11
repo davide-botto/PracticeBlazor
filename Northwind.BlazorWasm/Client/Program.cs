93c5893cbe0d7703190bb5f86045fe6448a5b0e2
Davide Botto <davidebotto.pro@gmail.com>
2022-09-11 23:11:00 +0200

    Aggiungere i file di progetto.

---
+++ Northwind.BlazorWasm/Client/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Northwind.BlazorWasm.Client;
+using Northwind.BlazorWasm.Client.Data;
+using Packt.Shared;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddNorthwindContext();
+builder.Services.AddTransient<INorthwindService, NorthwindService>();
+
+await builder.Build().RunAsync();
